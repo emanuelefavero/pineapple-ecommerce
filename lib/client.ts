ea9c7adb4128dd30ec0b33b6625ecb9b24e6e5fa
emanuelefavero <emafaux89@gmail.com>
2023-04-04 19:18:41 +0200

    update client.ts

--- lib/client.ts
+++ lib/client.ts
@@ -1,34 +1,13 @@
-// import sanityClient from '@sanity/client'
 import { createClient } from '@sanity/client'
 import imageUrlBuilder from '@sanity/image-url'
-
-// export const client = sanityClient({
-//   // run sanity manage to open the manage project webpage
-//   projectId: 'rnizwphe', // check project id in manage project homepage
-//   dataset: 'production', // check dataset name in Datasets tab
-
-//   // TODO: try to omit apiVersion
-//   // To check when a git project was created, run:
-//   // git log --reverse --format="%ai" | tail -1
-//   apiVersion: '2023-04-04', // when this project was created YYYY-MM-DD
-
-//   useCdn: true,
-
-//   // go to API tab in manage project homepage
-//   // and create a new token with read and write permissions (name it)
-//   token: process.env.NEXT_PUBLIC_SANITY_TOKEN,
-
-//   // This will ignore the browser warning about using the token in the client side (but since we are using .env.local and nextjs ssr we are safe)
-//   // sanity docs on this: https://www.sanity.io/help/js-client-browser-token
-//   ignoreBrowserTokenWarning: true,
-// })
 
 export const client = createClient({
   projectId: 'rnizwphe',
   dataset: 'production',
-  apiVersion: '2023-04-04',
-  useCdn: false, // ! TODO: turn this to true in production
+  apiVersion: '2023-04-04', // use current date
+  useCdn: false, // ! TODO: turn this to true in production if you want to use CDN cache
 
+  // NOTE: the token is only needed if the application updates data from the client side
   token: process.env.NEXT_PUBLIC_SANITY_TOKEN,
   //   ignoreBrowserTokenWarning: true,
 })
@@ -36,3 +15,7 @@ export const client = createClient({
 // use sanity images and access images urls
 const builder = imageUrlBuilder(client)
 export const urlFor = (source: any) => builder.image(source)
+
+// TIP: If you are using Next.js there is another sanity client that you can use
+// import { createClient } from 'next-sanity'
+// TODO: Try to use this client instead of the one above - @see https://www.sanity.io/docs/connect-your-content-to-next-js
