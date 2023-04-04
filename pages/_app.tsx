e96b3daf6e4c7b88a2182c07a824af5e70b8d1a2
emanuelefavero <emafaux89@gmail.com>
2023-04-04 11:11:20 +0200

    remove nextjs boilerplate

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,33 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import Head from 'next/head'
+import { Inter } from 'next/font/google'
+
+const inter = Inter({ subsets: ['latin'] })
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <div className={`${inter.className}`}>
+      <Head>
+        <title>Pineapple</title>
+        <meta
+          name='description'
+          content='An ecommerce selling the latest iPhones on all colors'
+        />
+        <meta name='viewport' content='width=device-width, initial-scale=1' />
+        <link rel='icon' href='/favicon.ico' />
+      </Head>
+
+      {/* HEADER */}
+      <header>Header</header>
+
+      {/* MAIN */}
+      <main>
+        <Component {...pageProps} />
+      </main>
+
+      {/* FOOTER */}
+      <footer>Footer</footer>
+    </div>
+  )
 }
