0abe64213b41260f6c09f0de3b6cb244cd6a9435
emanuelefavero <emafaux89@gmail.com>
2023-04-04 18:09:33 +0200

    create sanity schema

---
+++ sanity-pineapple/schemas/product.ts
@@ -0,0 +1,41 @@
+export default {
+  name: 'product',
+  title: 'Product',
+  type: 'document',
+  fields: [
+    {
+      name: 'image',
+      title: 'Image',
+      type: 'array',
+      of: [{type: 'image'}],
+      options: {
+        // hotspot is a boolean that allows you to focus on a certain part of the image when cropping
+        hotspot: true,
+      },
+    },
+    {
+      name: 'name',
+      title: 'Name',
+      type: 'string',
+    },
+    {
+      name: 'slug',
+      title: 'Slug',
+      type: 'slug', // Sanity has a slug type that generates a slug based on the value of another field
+      options: {
+        source: 'name', // The field to base the slug on
+        maxLength: 96,
+      },
+    },
+    {
+      name: 'price',
+      title: 'Price',
+      type: 'number',
+    },
+    {
+      name: 'details',
+      title: 'Details',
+      type: 'string',
+    },
+  ],
+}
