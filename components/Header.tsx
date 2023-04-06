f35098a1af44aa05d0be092f753c7230d42a3ef2
emanuelefavero <emafaux89@gmail.com>
2023-04-06 08:58:07 +0200

    style header

--- components/Header.tsx
+++ components/Header.tsx
@@ -10,18 +10,29 @@ export default function Header() {
     <>
       <div className='w-full flex justify-center items-center flex-col bg-white text-black'>
         <div
-          className={`${unbounded.className} select-none pt-1 pb-1 pr-1 pl-1 text-xl xs5:text-xl xs4:text-4xl xs4:pb-1 xs3:text-5xl xs3:pb-3 xs2:text-6xl xs2:pb-4 xs:text-7xl xs:pb-5 sm:text-8xl md:text-9xl`}
+          className={`${unbounded.className} select-none pt-1 pb-1 pr-1 pl-1 text-xl xs5:text-xl xs4:text-4xl xs4:pb-1 xs3:text-5xl xs3:pb-3 xs3:pt-2 xs2:text-6xl xs2:pb-4 xs:text-7xl xs:pb-5 sm:text-8xl md:text-9xl`}
         >
           {appName}
         </div>
-        <div className='w-full flex justify-between items-center border-t-2 border-b-2 border-black'>
-          <ul>
+        <div className='w-full flex justify-between items-center border-t-2 border-b-2 border-black font-medium p-0'>
+          <ul className='px-4 xs3:px-10'>
             <li>
-              <Link href='/'>Home</Link>
+              <Link
+                className='headerLink text-black hover:no-underline'
+                href='/'
+                id='homeLink'
+              >
+                Home
+              </Link>
             </li>
           </ul>
 
-          <button type='button'>Cart</button>
+          <button
+            className='bg-black text-white px-4 xs3:px-14 py-3 text-lg border-l-2 border-black hover:bg-indigo-500'
+            type='button'
+          >
+            Cart
+          </button>
         </div>
       </div>
     </>
