08388c9599e334c55b22411e493dab84d960c676
emanuelefavero <emafaux89@gmail.com>
2023-04-08 14:54:50 +0200

    add TODOs in Header.tsx

--- components/Header.tsx
+++ components/Header.tsx
@@ -9,6 +9,7 @@ const unbounded = Unbounded({ subsets: ['latin'] })
 export default function Header() {
   return (
     <>
+      {/* TODO: Add sticky position to Header */}
       <div className='w-full flex justify-center items-center flex-col bg-white text-black'>
         <div
           className={`${unbounded.className} select-none pt-1 pb-1 pr-1 pl-1 text-xl xs5:text-xl xs4:text-4xl xs4:pb-1 xs3:text-5xl xs3:pb-3 xs3:pt-2 xs2:text-6xl xs2:pb-4 xs:text-7xl xs:pb-5 sm:text-8xl md:text-9xl`}
