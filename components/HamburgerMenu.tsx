c07b77b772f7e0279345063f5a0ff1134a852c31
emanuelefavero <emafaux89@gmail.com>
2023-04-12 03:31:34 +0200

    fix responsive on mobiel in HamburgerMenu.tsx

--- components/HamburgerMenu.tsx
+++ components/HamburgerMenu.tsx
@@ -11,7 +11,7 @@ export default function HamburgerMenu() {
       {/* Hamburger Menu Overlay */}
       <section className='w-full h-full bg-slate-900 bg-opacity-80 fixed right-0 top-0 z-50 transition-all duration-1000 ease-in-out'>
         {/* Hamburger Menu */}
-        <div className='w-full xs2:w-10/12 sm:w-[37.5rem] h-full bg-indigo-200 float-right relative border-l-2 border-black'>
+        <div className='w-full xs2:w-10/12 sm:w-[37.5rem] h-full bg-indigo-200 float-right relative border-l-2 border-black overflow-y-scroll'>
           {/* HamburgerMenu Header */}
           <nav className='w-full bg-white px-4 py-4 border-b-2 border-black select-none flex justify-start items-center'>
             <button onClick={() => setShowHamburgerMenu(false)}>
@@ -19,8 +19,8 @@ export default function HamburgerMenu() {
             </button>
           </nav>
 
-          {/* Hamburger Menu */}
-          <ul className='px-7 flex flex-col justify-center items-center pt-10'>
+          {/* Hamburger Menu Items */}
+          <ul className='px-7 flex flex-col justify-center items-center pt-10 mb-6'>
             {/* TODO: Add real pages for these items (iPhone, Accessories, About Us) */}
             {/* TODO: Move to separate container where you only need to pass the menu item text and link url */}
             <li className='mr-5 mb-1'>
