c9cd1ce7e47cb9fd02d3f25110b8ce9d36efd374
emanuelefavero <emafaux89@gmail.com>
2023-04-14 04:06:52 +0200

    add content to success page

--- pages/success.tsx
+++ pages/success.tsx
@@ -1,6 +1,39 @@
 // TODO: Style success page
 // TODO: Add confetti animation
+import { IoBagCheckSharp } from 'react-icons/io5'
 
 export default function success() {
-  return <div>success</div>
+  return (
+    <div className='bg-indigo-200 px-10 pt-20 pb-40 flex justify-center items-center'>
+      <div className='bg-violet-300 border-2 border-black px-32 py-10 flex flex-col justify-center items-center'>
+        <IoBagCheckSharp className='w-16 h-16 text-green-700' />
+        <h1 className='text-4xl font-extrabold text-center wordSpacingTight tracking-tight'>
+          Thank You For Your Order!
+        </h1>
+        <p className='text-xl font-bold text-center wordSpacingTight tracking-tight'>
+          Check your email for the receipt
+        </p>
+        <p className='text-xl font-bold text-center wordSpacingTight tracking-tight'>
+          If you have any questions, please contact us at{' '}
+          <a
+            style={{
+              fontSize: '1.25rem',
+            }}
+            className='text-indigo-800 hover:underline'
+            href='mailto:info@emanuelefavero.com'
+          >
+            info@emanuelefavero.com
+          </a>
+        </p>
+
+        <button
+          className='
+          text-2xl font-bold select-none text-white bg-indigo-500 py-1 px-10 rounded-lg uppercase border-indigo-700 border-2 hover:bg-slate-800 hover:border-black active:border-white active:scale-95 transition-all duration-100 ease-in-out
+        '
+        >
+          Continue Shopping
+        </button>
+      </div>
+    </div>
+  )
 }
