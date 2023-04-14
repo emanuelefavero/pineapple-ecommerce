4f8fe92c0affa521eb4fb3690cd5623b0d4b287f
emanuelefavero <emafaux89@gmail.com>
2023-04-14 04:18:05 +0200

    add margins to success page card

--- pages/success.tsx
+++ pages/success.tsx
@@ -5,15 +5,15 @@ import { IoBagCheckSharp } from 'react-icons/io5'
 export default function success() {
   return (
     <div className='bg-indigo-200 px-10 pt-20 pb-40 flex justify-center items-center'>
-      <div className='bg-violet-300 border-2 border-black px-32 py-10 flex flex-col justify-center items-center'>
-        <IoBagCheckSharp className='w-16 h-16 text-green-700' />
-        <h1 className='text-4xl font-extrabold text-center wordSpacingTight tracking-tight'>
+      <div className='bg-violet-300 border-2 border-black px-32 py-12 flex flex-col justify-center items-center'>
+        <IoBagCheckSharp className='w-20 h-20 text-green-700 mb-4' />
+        <h1 className='text-4xl font-extrabold text-center wordSpacingTight tracking-tight mb-1'>
           Thank You For Your Order!
         </h1>
-        <p className='text-xl font-bold text-center wordSpacingTight tracking-tight'>
+        <p className='text-xl font-bold text-center wordSpacingTight tracking-tight mb-12'>
           Check your email for the receipt
         </p>
-        <p className='text-xl font-bold text-center wordSpacingTight tracking-tight'>
+        <p className='text-xl font-bold text-center wordSpacingTight tracking-tight mb-4'>
           If you have any questions, please contact us at{' '}
           <a
             style={{
